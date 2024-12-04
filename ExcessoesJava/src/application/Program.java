845551792cda6c62b7951552ed5e482d2e2b9609
jvitords <jvduarte.santos@hotmail.com>
2024-12-04 15:53:49 -0300

    Adicionando as pastas e classes

--- ExcessoesJava/src/application/Program.java
+++ ExcessoesJava/src/application/Program.java
@@ -1,9 +1,37 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import model.entities.Reserva;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("fff");
+		
+		Scanner digitar = new Scanner(System.in);
+		SimpleDateFormat formatoDaData = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Número do quarto: ");
+			Integer numeroDoQuarto = digitar.nextInt();
+			System.out.print("Data de CheckIn: ");
+			Date dataDoCheckIn = formatoDaData.parse(digitar.next());
+			System.out.print("Data de CheckOut: ");
+			Date dataDoCheckOut = formatoDaData.parse(digitar.next());
+			Reserva reserva = new Reserva(numeroDoQuarto, dataDoCheckIn, dataDoCheckOut);
+			System.out.println("Reserva: " + reserva);
+		}
+		
+		catch(InputMismatchException stringDigitadoNoLugarDoInteger) {
+			System.out.println("Número do quarto digitado de maneira errada: " + stringDigitadoNoLugarDoInteger);
+		}
+		
+		catch(ParseException tentativaDeDigitarData) {
+			System.out.println("Data digitada de maneira errada: " + tentativaDeDigitarData);
+		}
 	}
-
 }
